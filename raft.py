1a1241dbb739b6a42ff6224c010dc77fa7c78b90
David Nugent <david.nugent2425@gmail.com>
2021-03-04 10:35:10 +0000

    started python implementation by making constructor for Server class

---
+++ raft.py
@@ -0,0 +1,36 @@
+# implementation of the raft algorithm from the paper:
+# https://raft.github.io/raft.pdf
+
+class Server:
+    def __init__(self):
+        self.is_canditate = False
+        self.logs = []
+        self.is_leader = False
+        self.term_num = 1
+
+        # Variables used by all machines:
+
+        # latest term server has seen
+        self.current_term = 0
+        # candidate_id that received vote in current term
+        self.voted_for = None
+        # log entries; each entry contains a command and the term when
+        # the entry was received by the leader
+        self.log = []
+        # index of highest log entry known to be committed
+        self.commit_index = 0
+        # index of highest log entry that has been received
+        self.last_applied = 0
+        
+        # Variables used when the machine is a Leader:
+        # (all re-initialized after each election)
+        
+        # contains the indexes of the next log entry to be sent to each server
+        self.next_index = []
+        # contains the indexes of the highest log entry know to be replicated
+        # on each server
+        self.match_index = []
+
+
+if __name__ == '__main__':
+    server = Server();
